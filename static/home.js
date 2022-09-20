765dbf9ad6aafd6c0bfce74294ccecab45bd5310
suyeon woo <woosean999@gmail.com>
2022-09-20 16:30:00 +0900

    fix . 시간순 / 랜덤 버튼 클릭 선택이벤트 수정

--- static/home.js
+++ static/home.js
@@ -2,20 +2,25 @@ const sortTabArea = document.querySelector(".sort-tab-area");
 const sortByTime = document.querySelector(".by-time");
 const sortByRandom = document.querySelector(".by-random");
 
-sortTabArea.addEventListener(
+let currentSelectedSortBy = "time";
+sortByTime.addEventListener(
   "click",
-  (function selectSortOption() {
-    let selected = "time";
-    return () => {
-      if (selected === "time") {
-        selected = "random";
-        sortByRandom.classList.add("selected-sort-by");
-        sortByTime.classList.remove("selected-sort-by");
-      } else {
-        selected = "time";
-        sortByTime.classList.add("selected-sort-by");
-        sortByRandom.classList.remove("selected-sort-by");
-      }
-    };
-  })()
+  selectSortOption.bind(null, "time", currentSelectedSortBy)
 );
+
+sortByRandom.addEventListener(
+  "click",
+  selectSortOption.bind(null, "random", currentSelectedSortBy)
+);
+
+function selectSortOption(clicked) {
+  if (clicked === "time" && currentSelectedSortBy === "random") {
+    sortByTime.classList.add("selected-sort-by");
+    sortByRandom.classList.remove("selected-sort-by");
+    currentSelectedSortBy = "time";
+  } else if (clicked === "random" && currentSelectedSortBy === "time") {
+    sortByRandom.classList.add("selected-sort-by");
+    sortByTime.classList.remove("selected-sort-by");
+    currentSelectedSortBy = "random";
+  }
+}
