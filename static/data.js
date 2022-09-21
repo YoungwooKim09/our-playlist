11087c7c42429ff536bbbdf0b2d6a1002ad6fb54
kiwkim <atthenoon@gmail.com>
2022-09-22 02:43:58 +0900

    main.c

--- static/data.js
+++ static/data.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 console.log();
 
 const userInfo = (function () {
@@ -25,7 +25,3 @@ const userInfo = (function () {
   };
 })();
 
-console.log(userInfo.getUserInfo());
-=======
-var user_info;
->>>>>>> 45ff74c (최신화)
