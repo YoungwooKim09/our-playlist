c767efb9f62015bce5cac08ad8e96338a50b28d3
suyeon woo <woosean999@gmail.com>
2022-09-21 23:16:19 +0900

    feature . userinfo 쿠키에 저장

--- static/data.js
+++ static/data.js
@@ -1,1 +1,27 @@
-let user_info;
+console.log();
+
+const userInfo = (function () {
+  let userInfo = {
+    user_name: "",
+    user_email: "",
+  };
+  return {
+    setUserInfo(name, email) {
+      userInfo = {
+        user_name: name || userInfo.user_name,
+        user_email: email || userInfo.user_email,
+      };
+    },
+    removeUserInfo() {
+      userInfo = {
+        user_name: "",
+        user_email: "",
+      };
+    },
+    getUserInfo() {
+      return userInfo;
+    },
+  };
+})();
+
+console.log(userInfo.getUserInfo());
