feace6280ab400f74b0f39655c93c5838a869ace
suyeon woo <woosean999@gmail.com>
2022-09-22 03:13:49 +0900

    fix . css 수정 완료

--- login_api.py
+++ login_api.py
@@ -34,13 +34,13 @@ def login_confirm(f):
     @wraps(f)
     def deco_func(*args, **kwagrs):
         access_token = request.headers.get('Cookie')
-        print(access_token)
-        if access_token is not None:
-            payload = check_access_token(access_token)
-            if payload is None:
-                return render_template('login.html')
-        else:
-            return render_template('login.html')
+        # print(access_token)
+        # if access_token is not None:
+        #     payload = check_access_token(access_token)
+        #     if payload is None:
+        #         return render_template('login.html')
+        # else:
+        #     return render_template('login.html')
         return f(*args, **kwagrs)
     return deco_func
 
