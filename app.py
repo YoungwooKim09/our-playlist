735348df036bec95f50ba9308a6445ce07f7fcbf
suyeon woo <woosean999@gmail.com>
2022-09-21 19:52:50 +0900

    button draft - +버튼 버전

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request 
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -8,6 +8,10 @@ import routes
 
 client = MongoClient('localhost', 27017)
 db = client.testdb
+
+@app.route('/temp')
+def temp():
+    return render_template('myplaylist.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
