c43996d719cde8b307bc38b57d9b49a9b6c0032c
YoungwooKim <youngwoo9699@gmail.com>
2022-09-21 21:04:14 +0900

    home-api, my-api 21:00 수정

--- static/home-api.js
+++ static/home-api.js
@@ -3,6 +3,7 @@ let page = 1;
 $(document).ready(function () {
   $(".feed").html("");
   showAllplaylists(page);
+  
 });
 
 $(window).scroll(function () {
@@ -37,7 +38,6 @@ function showAllplaylists(page) {
 }
 
 function showPopularlist() {
-  
   $.ajax({
     type: "GET",
     url: "/list/popular",
@@ -47,88 +47,6 @@ function showPopularlist() {
       if (response["result"] == "success") {
         alert("포스팅 성공!");
         // 3. 성공 시 페이지 새로고침하기
-        window.location.reload();
-      } else {
-        alert("다시 입력하세요!");
-      }
-    },
-  });
-}
-
-
-function makeList(index, user, title, songs) {
-  let tempHtml_pl = `<li>
-                      <div class="playlist-block playlist-block${index}">
-                        <p class="area-title area-title${index}">${title} by ${user}</p>
-                        <ul class="songs${index}"></ul>
-                      </div>
-                    </li>`;
-  $(".feed").append(tempHtml_pl);
-
-  if (!songs || songs.length === 0) return;
-  for (let j = 0; j < 3; j++) {
-    let song_name = songs[j]["songname"];
-    let song_artist = songs[j]["artist"];
-
-    let tempHtml_s = `<li>${j + 1}. ${song_name} - ${song_artist}</li>`;
-
-    $(`.songs${index}`).append(tempHtml_s);
-  }
-  let spreadButtonHtml = `<button class="spread-button spread-button${index}"><i class="fa-solid fa-caret-right"></i> 펼쳐보기 </button>`;
-  $(`.playlist-block${index}`).append(spreadButtonHtml);
-
-  const toggleSpreadButton = (function () {
-    let spread = false;
-    let showCount = 3;
-    return function () {
-      spread = !spread;
-      if (spread) {
-        spreadButton.innerHTML = `<i class="fa-solid fa-caret-down"></i> 접기`;
-        showCount = songs.length;
-        toggle("fold", "spread", showCount);
-      } else {
-        spreadButton.innerHTML = `<i class="fa-solid fa-caret-right"></i> 펼쳐보기`;
-        showCount = 3;
-        toggle("spread", "fold", showCount);
-      }
-    };
-  })();
-
-  let spreadButton = document.querySelector(`.spread-button${index}`);
-  spreadButton.addEventListener("click", toggleSpreadButton.bind(null, index));
-
-  function toggle(currentState, nextState, showCount) {
-    let start;
-    if (currentState === "fold" && nextState === "spread") {
-      start = 3;
-    } else if (currentState === "spread" && nextState === "fold") {
-      start = 0;
-      $(`.songs${index}`).html("");
-    }
-
-    for (let j = start; j < showCount; j++) {
-      let song_name = songs[j]["songname"];
-      let song_artist = songs[j]["artist"];
-      let tempHtml_s = `<li>${j + 1}. ${song_name} - ${song_artist}</li>`;
-
-      $(`.songs${index}`).append(tempHtml_s);
-    }
-  }
-}
-
-function addPlaylist() {
-  let user = $("#add-user").val();
-  let title = $("#add-playlist").val();
-
-  $.ajax({
-    type: "GET",
-    url: "/list/popular",
-    data: {},
-    success: function (response) {
-      
-      if (response["result"] == "success") {
-        alert("플레이 리스트 만들기 성공!");
-        
         window.location.reload();
       } else {
         alert("다시 입력하세요!");
@@ -208,8 +126,8 @@ function addPlaylist() {
     success: function (response) {
       if (response["result"] == "success") {
         alert("플레이 리스트 만들기 성공!");
-
         window.location.reload();
+
       } else {
         alert("다시 입력하세요!");
       }
--- static/myplay-api.js
+++ static/myplay-api.js
@@ -1,62 +1,66 @@
 
 function deletePlaylist(index) {
-    
-    let delete_title = $("." + `area-title${index}`).text;
 
-    $.ajax({
-      type: "POST",
-      url: "/delete/playlist",
-      data: { delete_give: delete_title },
-      success: function (response) { // 성공하면
-        if (response["result"] == "success") {
-          
-          // 3. 성공 시 페이지 새로고침하기
-          // window.location.reload();
-          alert("플레이 리스트 삭제 완료!")
-        } else {
-          alert("다시 입력하세요!")
-        }
+  let delete_title = $(".area-title" + index).text();
+
+  $.ajax({
+    type: "POST",
+    url: "/delete/playlist",
+    data: { delete_give: delete_title },
+    success: function (response) {
+      if (response["result"] == "success") {
+
+        alert("플레이 리스트 삭제 완료!")
+        window.location.reload();
+
+      } else {
+        alert("다시 입력하세요!")
       }
-    })
-  }
+    }
+  })
+}
 
 function searchSong() {
-    let search_song = $("#search-song").val();
-    
-    $.ajax({
-      type: "POST",
-      url: "/search",
-      data: { search_give: search_song },
-      success: function (response) { // 성공하면
-        if (response["result"] == "success") {
-          console.log(response['search_song_name_list']);
-          // 3. 성공 시 페이지 새로고침하기
-          // window.location.reload();
-        } else {
-          alert("다시 입력하세요!")
+  let search_song = $("#search-song").val();
+
+  $.ajax({
+    type: "POST",
+    url: "/search",
+    data: { search_give: search_song },
+    success: function (response) {
+      if (response["result"] == "success") {
+        console.log(response['song']);
+        searchList = response['song'];
+        for (let i = 0; i < searchList.length; i++) {
+          makeSearchList(i + 1, searchList[i]['song_name'], searchList[i]['song_singer']);
         }
       }
-    })
-  }
+    }
+  })
+}
 
-  function addSong() {
-    let song = $("#add-song").text();
-    let artist = $("#add-artist").text();
-    // 버튼에 해당하는 텍스트 요소
-    // 함수 인자로 id값 사용
+function makeSearchList(index, songName, songArtist) {
+  let tempHtml
+}
 
-    $.ajax({
-      type: "POST",
-      url: "/add/song",
-      data: { song_give: song, artist_give: artist },
-      success: function (response) { // 성공하면
-        if (response["result"] == "success") {
-          alert("포스팅 성공!");
-          // 3. 성공 시 페이지 새로고침하기
-          // window.location.reload();
-        } else {
-          alert("다시 입력하세요!")
-        }
+function addSong() {
+  let song = $("#add-song").text();
+  let artist = $("#add-artist").text();
+  // 버튼에 해당하는 텍스트 요소
+  // 함수 인자로 id값 사용
+
+  $.ajax({
+    type: "POST",
+    url: "/add/song",
+    data: { song_give: song, artist_give: artist },
+    success: function (response) { // 성공하면
+      if (response["result"] == "success") {
+        alert("포스팅 성공!");
+        // 3. 성공 시 페이지 새로고침하기
+        // window.location.reload();
+      } else {
+        alert("다시 입력하세요!")
       }
-    })
-  }
+    }
+  })
+}
